bfe570d3ce53eb532a169654efa812483513bdaa
Chirala Mounika <chiralamounikachowdary24@gmail.com>
2026-05-18 11:20:26 +0530

    Add _redirects file for React Router

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Countries from "./pages/Countries";
-import Places from "./pages/Places";
+import Places from "./pages/places";
 import Details from "./pages/Details";
 import Booking from "./pages/Booking";
 import Confirmation from "./pages/Confirmation";
